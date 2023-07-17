7d14f534c870feb4dbce178a659d299d259bf32a
Matthew Gapp <61894094+matthewgapp@users.noreply.github.com>
2023-07-16 18:21:22 -0700

    expand type based on what was logged

--- src/models/oauth/token.rs
+++ src/models/oauth/token.rs
@@ -1,12 +1,17 @@
 use serde::{Deserialize, Serialize};
 
+use crate::models::users::User;
+
 #[derive(Serialize, Deserialize, Debug, Eq, PartialEq, Clone)]
 pub struct Token {
     pub access_token: String,
     pub bot_id: String,
+    /// not documented
+    pub token_type: Option<String>,
     pub duplicated_template_id: Option<String>,
     pub owner: Owner,
-    pub workspace_icon: String,
+    /// this can be null which isn't documented
+    pub workspace_icon: Option<String>,
     pub workspace_id: String,
     pub workspace_name: String,
 }
@@ -14,4 +19,7 @@ pub struct Token {
 #[derive(Serialize, Deserialize, Debug, Eq, PartialEq, Clone)]
 pub struct Owner {
     workspace: bool,
+    // TODO: should be an enum
+    r#type: String,
+    user: Option<User>,
 }
